5a1ffa01dea07ce9c9459ff85a2bf39332cc7f86
Marcus T <marcust261@icloud.com>
2016-02-14 15:39:15 +0000

    Refactor forMonth() as forMonthContaining().
    Instead of forMonth() taking a date, it could be refactored to take a year and month (integers); to keep the ability to provide a date within the month, i have created a method called forMonthContaining().
    
    The forMonthContaining() method could also accept a Carbon date instance as well as the previous string and format.

---
+++ src/Exceptions/DateRangeException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace _20TRIES\Exceptions;
+
+/**
+ * An exception that concerns a date range.
+ *
+ * @author Marcus T <marcus.turner@creare.uk>
+ *
+ * @since v0
+ */
+class DateRangeException extends \Exception
+{
+}
