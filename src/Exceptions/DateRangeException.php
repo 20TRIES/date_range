606244b57a4db914b743ddff7f02d400b517e4e7
Marcus T <marcust261@icloud.com>
2016-02-14 14:25:27 +0000

    Updated constructor to assume timezone from the provided date objects.

---
+++ src/Exceptions/DateRangeException.php
@@ -0,0 +1,13 @@
+<?php namespace _20TRIES\Exceptions;
+
+/**
+ * An exception that concerns a date range.
+ *
+ * @package _20TRIES
+ * @author Marcus T <marcus.turner@creare.uk>
+ * @since v0
+ */
+class DateRangeException extends \Exception
+{
+
+}
