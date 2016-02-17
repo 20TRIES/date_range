4325a44e38f3e141ed7456c0ed8bb1c34608de20
Marcus T <marcust261@icloud.com>
2016-02-17 17:27:05 +0000

    Added tests for "getAfter" and "getBefore" methods to makes sure that the object date instances are not returned and that the datetime values are returned correctly.

--- tests/GettersTest.php
+++ tests/GettersTest.php
@@ -14,6 +14,18 @@ use Carbon\Carbon;
  */
 class GettersTest extends \PHPUnit_Framework_TestCase
 {
+    public function test_getAfter_returns_a_new_carbon_instance_with_correct_value()
+    {
+        $range = new DateRange(
+            $after = Carbon::today()->subSecond(),
+            Carbon::today()->endOfDay()->addSecond()
+        );
+
+        $this->assertEquals(Carbon::today()->subSecond(), $range->getAfter());
+
+        $this->assertTrue($after !== $range->getAfter());
+    }
+
     public function test_getFrom_returns_a_new_carbon_instance_with_correct_value()
     {
         $range = new DateRange(
@@ -24,6 +36,18 @@ class GettersTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals(Carbon::today(), $range->getFrom());
 
         $this->assertTrue($after !== $range->getFrom());
+    }
+
+    public function test_getBefore_returns_a_new_carbon_instance_with_correct_value()
+    {
+        $range = new DateRange(
+            Carbon::today()->subSecond(),
+            $before = Carbon::today()->endOfDay()->addSecond()
+        );
+
+        $this->assertEquals(Carbon::today()->endOfDay()->addSecond(), $range->getBefore());
+
+        $this->assertTrue($before !== $range->getBefore());
     }
 
     public function test_getTo_returns_a_new_carbon_instance_with_correct_value()
